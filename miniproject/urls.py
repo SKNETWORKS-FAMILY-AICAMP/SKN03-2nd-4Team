7eb68ca29bce1920da9719c47c4db64a6e98a6ad
artemIntellectus <smoh17@naver.com>
2024-08-19 09:40:54 +0900

    feat: 각 페이지별 링크 연결 및 html 수정

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mainpage_app.urls")),
-    path("weather", include("weather_app.urls")),
+    path("weather/", include("weather_app.urls")),
     path("keyword/", include("keyword_app.urls")),
     path("visitor_region/", include("visitor_region_app.urls")),
     path("visitor_jeju/", include("visitor_jeju_app.urls")),
@@ -12,5 +12,4 @@ urlpatterns = [
     path("consumption_region/", include("consumption_region_app.urls")),
     path("consumption_industry/", include("consumption_industry_app.urls")),
     path("map/", include("map_app.urls")),
-    path("map_test/", include("map_test.urls")),
 ]
